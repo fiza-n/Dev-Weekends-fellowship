f88ffa484400bfaa0f4f11bf35542703fc4a5688
fiza noor <fnoor034726@gmail.com>
2026-06-13 14:12:21 +0500

    changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { navLinks } from "../constants/index";
 import "./Navbar.css";
+import close from "../assets/close.png"
+import hamburger from "../assets/hamburger.png"
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen]= useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen((prev) => !prev);
+  }
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <h1>Trippy</h1>
       </div>
 
-      <div className="navbar-links-container">
+      <div className="navbar-menu-toggle">
+        <button onClick={handleMenuToggle}>
+          <img src={isMenuOpen ? close : hamburger} alt="Menu" />
+        </button>
+      </div>
+
+      <div className={`navbar-links-container ${isMenuOpen ? 'open' : ''}`}>
         <ul className="navbar-links">
           {navLinks.map((l) => (
             <li key={l.title}>
               <NavLink
                 to={l.url}
+                onClick={() => setIsMenuOpen(false)}
                 className={({ isActive }) =>
                   isActive ? "navbar-link active" : "navbar-link"
                 }
